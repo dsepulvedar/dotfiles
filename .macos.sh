406ee3d602b8a014cc014740981c574b9a75d85f
David Sepúlveda Ramírez <dsepulvedara@gmail.com>
2020-06-25 18:11:51 -0500

    macos and vimrc

--- .macos.sh
+++ .macos.sh
@@ -16,6 +16,9 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 echo "Hello $(whoami)! Let's get you set up."
 
+echo "installing Apple CommandLineTools"
+xcode-select --install
+
 echo "installing homebrew"
 # install homebrew https://brew.sh
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -29,5 +32,27 @@ echo "brew installing stuff"
 # mosquitto: MQTT client 
 # ripgrep: rg is faster than alternatives
 # unixodbc: ODBC 3 connectivity for UNIX
+# zsh: UNIX shell
 brew install dfu-util freetds git htop jq mosquitto ripgrep \
-unixodbc
+unixodbc zsh
+
+
+echo "installing apps with brew cask"
+# Apps needed not available through brew cask install nor tap:
+# Paste, MS Office, GoPro Quick, PlayMemories Home, Giphy Capture
+# Adobe Lr Ps Pr
+brew cask install google-chrome firefox brave-browser \
+visual-studio-code arduino 1password balenaetcher insomnia\
+zoomus iterm2 \
+spotify whatsapp zappy
+
+echo "Making zsh the default shell"
+chsh -s /usr/local/bin/zsh
+
+# Logic here to close session, open again in Iterm2 and continue with script
+
+echo "Installing oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+
---
+++ .vimrc
@@ -0,0 +1,18 @@
+" DSR VIM personal configuration
+
+set nocompatible " VI compatible mode is disabled so that VIm things work
+
+syntax on           " Enable syntax processing
+
+" Spaces & Tabs
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+set tabstop=4       " number of visual spaces per TAB
+set softtabstop=4   " number of spaces in tab when editing
+set shiftwidth=4    " Insert 4 spaces on a tab
+set expandtab       " tabs are spaces, mainly because of python
+
+" UI Config
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+set number              " show line numbers
+set relativenumber      " show relative numbering
+set showcmd             " show command in bottom bar
