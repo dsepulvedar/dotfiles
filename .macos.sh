ea7917b77c00b37595ca8db9aae018524b009ef4
David Sepúlveda Ramírez <dsepulvedara@gmail.com>
2020-06-25 19:05:42 -0500

    macos

--- .macos.sh
+++ .macos.sh
@@ -38,9 +38,6 @@ unixodbc zsh
 
 
 echo "installing apps with brew cask"
-# Apps needed not available through brew cask install nor tap:
-# Paste, MS Office, GoPro Quick, PlayMemories Home, Giphy Capture
-# Adobe Lr Ps Pr
 brew cask install google-chrome firefox brave-browser \
 visual-studio-code arduino 1password balenaetcher insomnia\
 zoomus iterm2 \
@@ -51,8 +48,84 @@ chsh -s /usr/local/bin/zsh
 
 # Logic here to close session, open again in Iterm2 and continue with script
 
-echo "Installing oh-my-zsh"
+echo "installing oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+echo "instaling pyenv"
+curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+
+echo "installing nvm"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+###############################################################################
+# Trackpad, mouse, keyboard, Bluetooth accessories, and input                 #
+###############################################################################
+
+# Trackpad: enable tap to click for this user and for the login screen
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+# Trackpad: enable three finger drag
+defaults write http://com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -int 1
+
+# Disable “natural” (Lion-style) scrolling
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
 
 
+
+
+
+###############################################################################
+# Screen                                                                      #
+###############################################################################
+
+# Require password immediately after sleep or screen saver begins
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+# Save screenshots to the Desktop/Screenshots
+defaults write com.apple.screencapture location -string "${HOME}/Desktop/Screenshots"
+
+# Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
+defaults write com.apple.screencapture type -string "png"
+
+# Disable shadow in screenshots
+# defaults write com.apple.screencapture disable-shadow -bool true
+
+# Enable subpixel font rendering on non-Apple LCDs
+# Reference: https://github.com/kevinSuttle/macOS-Defaults/issues/17#issuecomment-266633501
+defaults write NSGlobalDomain AppleFontSmoothing -int 1
+
+# Enable HiDPI display modes (requires restart)
+# sudo defaults write /Library/Preferences/com.apple.windowserver DisplayResolutionEnabled -bool true
+
+
+
+# Show the ~/Library folder
+chflags nohidden ~/Library
+
+# Don’t display the annoying prompt when quitting iTerm
+defaults write com.googlecode.iterm2 PromptOnQuit -bool false
+
+
+###############################################################################
+# Photos                                                                      #
+###############################################################################
+
+# Prevent Photos from opening automatically when devices are plugged in
+defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
+
+
+
+printf "TODO:\n\
+install: \n\
+  Paste (App Store) \n\
+  MS Office \n\
+  Adobe Lr, Ps, Pr \n\
+  GoPro Quick (https://community.gopro.com/t5/en/GoPro-legacy-software/ta-p/595533) \n\
+  PlayMemories Home \n\
+  Giphy Capture \n\
+\n\
+Restart Terminal.app\n\
+login to literally everything \n\
+"
